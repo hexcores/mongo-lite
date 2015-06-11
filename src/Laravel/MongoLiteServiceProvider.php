0c4ea25ee7ce82d6bff3e71a99b447a9fdf67e8c
Nyan Lynn Htut <lynnhtut87@gmail.com>
2015-06-11 17:16:55 +0630

    Add ServiceProvider for Laravel

--- src/Laravel/MongoLiteServiceProvider.php
+++ src/Laravel/MongoLiteServiceProvider.php
@@ -1,12 +1,33 @@
 <?php namespace Hexcores\MongoLite\Laravel;
 
 use Illuminate\Support\ServiceProvider;
+use Hexcores\MongoLite\Connection;
+use Hexcores\MongoLite\Query;
 
 class MongoLiteServiceProvider extends ServiceProvider
 {
+	/**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    	$config = $this->app['config']->get('mongo_lite');
 
-	public function register()
-	{
-		
-	}
+        Connection::connect(
+        	$config['host'], 
+        	$config['port'], 
+        	$config['database'], 
+        	$config['user'], 
+        	$config['password']
+        );
+    }
+
+	/**
+     * Register the service provider.
+     *
+     * @return void
+     */
+	public function register() {}
 }
